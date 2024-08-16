6b826f9435195bf460fed7148545bd1768889f7a
James Hodson <ir-hods1@login-q-3.data.cluster>
2024-08-16 13:40:22 +0100

    fixed grouping issue, now metadata workflow is grouped

--- src/dags/ingestion_dag.py
+++ src/dags/ingestion_dag.py
@@ -2,6 +2,7 @@ from pendulum import datetime
 import logging
 from pathlib import Path
 import sys
+from airflow import DAG
 sys.path.append("/home/ir-hods1/projects/fair-mast-ingestion")
 from src.utils import read_shot_file
 from src.workflow import CreateSignalMetadataTask, CreateSourceMetadataTask
@@ -17,17 +18,18 @@ def signal_metadata_workflow(data_dir: str, shot: int):
 
 def source_metadata_workflow(data_dir: str, shot: int):
     try:
-        source_metadata = CreateSourceMetadataTask(f"{data_dir}/signals", shot)
+        source_metadata = CreateSourceMetadataTask(f"{data_dir}/source", shot)
         source_metadata()
     except Exception as e:
         logging.error(f"Could not parse source metadata for shot {shot}: {e}") 
 
+@task
 def run_workflows_serial_signal(data_dir, shot_list: list):
         n = len(shot_list)
         for i, shot in enumerate(shot_list):
             signal_metadata_workflow(data_dir, shot)
             logging.info(f"Done shot {i+1}/{n} = {(i+1)/n*100:.2f}%")
-
+@task
 def run_workflows_serial_source(data_dir, shot_list: list):
         n = len(shot_list)
         for i, shot in enumerate(shot_list):
@@ -35,25 +37,17 @@ def run_workflows_serial_source(data_dir, shot_list: list):
             logging.info(f"Done shot {i+1}/{n} = {(i+1)/n*100:.2f}%")
 
 default_args = {"start_date": datetime(2021, 1, 1)}
-@dag(schedule="@daily", default_args=default_args, catchup=False)
-def create_uda_metadata():
-    
+with DAG('metadata_processing', default_args=default_args, schedule_interval='@daily') as dag:
     @task
     def read_shots(shot_file):
         shot_list = read_shot_file(shot_file)
         return shot_list
-
-    @task
-    def signal_metadata_workflow(data_dir, shot_list):
-        run_workflows_serial_signal(data_dir, shot_list)
-        
-
-    @task
-    def source_metadata_workflow(data_dir, shot_list):
+    
+    @task_group
+    def metadata_workflow_group(data_dir, shot_list):
+         run_workflows_serial_signal(data_dir, shot_list)
          run_workflows_serial_source(data_dir, shot_list)
 
-    shots = read_shots(shot_file="/home/ir-hods1/projects/fair-mast-ingestion/campaign_shots/tiny_campaign.csv")
-    signal = signal_metadata_workflow(data_dir="/home/ir-hods1/projects/fair-mast-ingestion/data/uda", shot_list=shots)
-    source = source_metadata_workflow(data_dir="/home/ir-hods1/projects/fair-mast-ingestion/data/uda", shot_list=shots)
 
-create_uda_metadata()
+    shots = read_shots(shot_file="/home/ir-hods1/projects/fair-mast-ingestion/campaign_shots/tiny_campaign.csv")
+    metadata_workflow_group(data_dir="/home/ir-hods1/projects/fair-mast-ingestion/data/uda", shot_list=shots)

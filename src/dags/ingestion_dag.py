d8572f4a757e83da678e8259fe62549fd9142ff9
James Hodson <ir-hods1@login-q-1.data.cluster>
2024-08-21 09:21:42 +0100

    update dag

--- src/dags/ingestion_dag.py
+++ src/dags/ingestion_dag.py
@@ -1,15 +1,10 @@
 from pendulum import datetime
 import logging
-from pathlib import Path
-import sys
 from airflow import DAG
-sys.path.append("/home/ir-hods1/projects/fair-mast-ingestion")
-from src.utils import read_shot_file
-from src.workflow import CreateSignalMetadataTask, CreateSourceMetadataTask
-from src.workflow import WorkflowManager, MetadataWorkflow
 from airflow.decorators import dag, task, task_group
 
 def signal_metadata_workflow(data_dir: str, shot: int):
+    from src.workflow import CreateSignalMetadataTask
     try:
         signal_metadata = CreateSignalMetadataTask(f"{data_dir}/signals", shot)
         signal_metadata()
@@ -17,31 +12,34 @@ def signal_metadata_workflow(data_dir: str, shot: int):
         logging.error(f"Could not parse signal metadata for shot {shot}: {e}")   
 
 def source_metadata_workflow(data_dir: str, shot: int):
+    from src.workflow import CreateSourceMetadataTask
     try:
         source_metadata = CreateSourceMetadataTask(f"{data_dir}/source", shot)
         source_metadata()
     except Exception as e:
         logging.error(f"Could not parse source metadata for shot {shot}: {e}") 
 
-@task
-def run_workflows_serial_signal(data_dir, shot_list: list):
+
+default_args = {"start_date": datetime(2021, 1, 1)}
+with DAG('metadata_processing', default_args=default_args, schedule_interval=None) as dag:
+    @task
+    def read_shots(shot_file):
+        from src.utils import read_shot_file
+        shot_list = read_shot_file(shot_file)
+        return shot_list
+    
+    @task
+    def run_workflows_serial_signal(data_dir, shot_list: list):
         n = len(shot_list)
         for i, shot in enumerate(shot_list):
             signal_metadata_workflow(data_dir, shot)
             logging.info(f"Done shot {i+1}/{n} = {(i+1)/n*100:.2f}%")
-@task
-def run_workflows_serial_source(data_dir, shot_list: list):
+    @task
+    def run_workflows_serial_source(data_dir, shot_list: list):
         n = len(shot_list)
         for i, shot in enumerate(shot_list):
             source_metadata_workflow(data_dir, shot)
             logging.info(f"Done shot {i+1}/{n} = {(i+1)/n*100:.2f}%")
-
-default_args = {"start_date": datetime(2021, 1, 1)}
-with DAG('metadata_processing', default_args=default_args) as dag:
-    @task
-    def read_shots(shot_file):
-        shot_list = read_shot_file(shot_file)
-        return shot_list
     
     @task_group
     def metadata_workflow_group(data_dir, shot_list):

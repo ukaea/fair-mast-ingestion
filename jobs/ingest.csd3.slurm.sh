4964f25eea4792a28f02373a88f58d6a55cd2d2a
Samuel Jackson <samueljackson@outlook.com>
2024-10-03 14:58:52 +0100

    Update job scripts

--- jobs/ingest.csd3.slurm.sh
+++ jobs/ingest.csd3.slurm.sh
@@ -9,8 +9,7 @@
 #SBATCH -N 2
 
 
-summary_file=$1
-bucket_path=$2
+bucket_path=$1
 num_workers=$SLURM_NTASKS
 
 random_string=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
@@ -19,5 +18,18 @@ metadata_dir="/rds/project/rds-sPGbyCAPsJI/data/uda"
 
 source /rds/project/rds-sPGbyCAPsJI/uda-ssl.sh
 
+summary_file="./campaign_shots/M9.csv"
 mpirun -np $num_workers \
-    python3 -m src.main $temp_dir $summary_file --metadata_dir $metadata_dir --bucket_path $bucket_path --upload --force --source_names ${@:3}
+    python3 -m src.main $temp_dir $summary_file --metadata_dir $metadata_dir --bucket_path $bucket_path --upload --force --source_names ${@:2}
+summary_file="./campaign_shots/M8.csv"
+mpirun -np $num_workers \
+    python3 -m src.main $temp_dir $summary_file --metadata_dir $metadata_dir --bucket_path $bucket_path --upload --force --source_names ${@:2}
+summary_file="./campaign_shots/M7.csv"
+mpirun -np $num_workers \
+    python3 -m src.main $temp_dir $summary_file --metadata_dir $metadata_dir --bucket_path $bucket_path --upload --force --source_names ${@:2}
+summary_file="./campaign_shots/M6.csv"
+mpirun -np $num_workers \
+    python3 -m src.main $temp_dir $summary_file --metadata_dir $metadata_dir --bucket_path $bucket_path --upload --force --source_names ${@:2}
+summary_file="./campaign_shots/M5.csv"
+mpirun -np $num_workers \
+    python3 -m src.main $temp_dir $summary_file --metadata_dir $metadata_dir --bucket_path $bucket_path --upload --force --source_names ${@:2}
--- jobs/metadata.csd3.slurm.sh
+++ jobs/metadata.csd3.slurm.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #SBATCH -A UKAEA-AP002-CPU
 #SBATCH -p icelake
-#SBATCH --job-name=fair-mast-ingest
-#SBATCH --output=%A_%a.out
-#SBATCH --time=0:20:00
+#SBATCH --job-name=fair-mast-metadata
+#SBATCH --output=fair-mast-metadata-%A.out
+#SBATCH --time=1:00:00
 #SBATCH --mem=60G
-#SBATCH --ntasks=128
-#SBATCH -N 2
+#SBATCH --ntasks=64
+#SBATCH -N 1
 
 num_workers=$SLURM_NTASKS
 
 uda_path="/rds/project/rds-sPGbyCAPsJI/data/uda"
+s3_path="/rds/project/rds-sPGbyCAPsJI/data/s3"
+index_path=./data/index
+
 source /rds/project/rds-sPGbyCAPsJI/uda-ssl.sh
+mkdir -p $index_path
 
 # Parse Signal and Source metadata from UDA
 mpirun -n $num_workers python3 -m src.create_uda_metadata $uda_path campaign_shots/M9.csv 
@@ -20,3 +24,8 @@ mpirun -n $num_workers python3 -m src.create_uda_metadata $uda_path campaign_sho
 mpirun -n $num_workers python3 -m src.create_uda_metadata $uda_path campaign_shots/M6.csv 
 mpirun -n $num_workers python3 -m src.create_uda_metadata $uda_path campaign_shots/M5.csv 
 
+mpirun -n $num_workers python3 -m src.read_source_metadata $uda_path/sources s3://mast/level1/shots $s3_path/sources
+mpirun -n $num_workers python3 -m src.read_signal_metadata $uda_path/sources s3://mast/level1/shots $s3_path/signals
+
+python3 -m src.group_parquet $s3_path/sources/ $index_path/sources.parquet
+python3 -m src.group_parquet $s3_path/signals/ $index_path/signals.parquet

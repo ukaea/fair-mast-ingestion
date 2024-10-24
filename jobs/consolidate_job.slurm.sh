916823f4f6fc035d3687267d95bb7ef56eabb3db
James Hodson <ir-hods1@login-q-3.data.cluster>
2024-10-24 08:21:20 +0100

    job script to run consolidation on csd3

---
+++ jobs/consolidate_job.slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -A UKAEA-AP002-CPU
+#SBATCH -p icelake
+#SBATCH --job-name=fair-mast-ingest
+#SBATCH --output=%A_%a.out
+#SBATCH --time=24:00:00
+#SBATCH --mem=60G
+#SBATCH --ntasks=256
+#SBATCH -N 4
+
+num_workers=$SLURM_NTASKS
+
+mpirun -n $num_workers python3 -m consolidate_s3

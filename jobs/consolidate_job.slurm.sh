83343bedcb9628810e57e2197aa4d4a567e0a94a
James Hodson <ir-hods1@login-q-2.data.cluster>
2024-10-25 10:15:31 +0100

    extend time limit

--- jobs/consolidate_job.slurm.sh
+++ jobs/consolidate_job.slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH -p icelake
 #SBATCH --job-name=fair-mast-ingest
 #SBATCH --output=%A_%a.out
-#SBATCH --time=24:00:00
+#SBATCH --time=36:00:00
 #SBATCH --mem=60G
 #SBATCH --ntasks=256
 #SBATCH -N 4

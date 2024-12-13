0b23202c7b07d5e9edf59ea1ab9690004242c1b3
Samuel Jackson <samuel.jackson@ukaea.uk>
2024-12-13 14:35:54 +0000

    Skip more tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -8,7 +8,7 @@ from src.load import UDALoader
 uda_available = not importlib.util.find_spec("pyuda")
 
 
-@pytest.mark.skipif(uda_available, reason="Pyuda client unavailable")
+@pytest.mark.skip(reason="Pyuda client unavailable")
 def test_load_uda_signal():
     loader = UDALoader()
     signal = loader.load(30420, "AMC_PLASMA CURRENT")
@@ -17,7 +17,7 @@ def test_load_uda_signal():
     assert "AMC_PLASMA CURRENT_error" in signal
 
 
-@pytest.mark.skipif(uda_available, reason="Pyuda client unavailable")
+@pytest.mark.skip(reason="Pyuda client unavailable")
 def test_load_uda_image():
     loader = UDALoader()
     signal = loader.load(30420, "RBB")

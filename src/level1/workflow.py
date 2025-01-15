8a8bb796fd210100e8baca98e62401fb4abbbfa8
Samuel Jackson <samuel.jackson@ukaea.uk>
2025-01-15 08:50:17 +0000

    Fix imports

--- src/level1/workflow.py
+++ src/level1/workflow.py
@@ -2,13 +2,13 @@ import traceback
 from pathlib import Path
 from typing import Optional
 
-from src.core.builder import DatasetBuilder
 from src.core.config import IngestionConfig
 from src.core.load import loader_registry
 from src.core.log import logger
 from src.core.metadata import MetadataWriter
 from src.core.upload import UploadS3
 from src.core.writer import dataset_writer_registry
+from src.level1.builder import DatasetBuilder
 from src.level1.pipelines import pipelines_registry
 
 

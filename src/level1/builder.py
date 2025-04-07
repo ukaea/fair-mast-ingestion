4c6aa78a24d346ff283c637b0c09ac743ecc24f9
Your GitHub Username <james.hodson@ukaea.uk>
2025-04-07 11:29:36 +0100

    added license

--- src/level1/builder.py
+++ src/level1/builder.py
@@ -56,6 +56,7 @@ class DatasetBuilder:
             dataset.attrs["name"] = group_name
             dataset.attrs["description"] = dataset_info.description
             dataset.attrs["quality"] = dataset_info.quality
+            dataset.attrs["license"] = "Creative Commons 4.0 BY-SA"
 
             logger.info(f"Writing {group_name} for shot #{shot}")
             file_name = f"{shot}.{self.writer.file_extension}"
@@ -160,6 +161,7 @@ class DatasetBuilder:
                 dataset.attrs["name"] = name
                 dataset.attrs["source"] = group_name
                 dataset.attrs["quality"] = signal_info.quality
+                dataset.attrs["license"] = "Creative Commons 4.0 BY-SA"
                 datasets[name] = dataset
             except MissingProfileError as e:
                 if "StructuredData" not in str(e):

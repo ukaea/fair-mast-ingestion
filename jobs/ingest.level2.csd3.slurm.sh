a044c92e16c5ea13a153de92b5ec37e66880ca9f
Samuel Jackson <samueljackson@outlook.com>
2025-06-26 08:05:32 +0100

    Update jobscript

--- jobs/ingest.level2.csd3.slurm.sh
+++ jobs/ingest.level2.csd3.slurm.sh
@@ -12,7 +12,8 @@
 num_workers=$SLURM_NTASKS
 source .venv/bin/activate
 source /rds/project/rds-mOlK9qn0PlQ/fairmast/uda-ssl.sh
+output_dir="~/rds/rds-ukaea-ap002-mOlK9qn0PlQ/fairmast/upload-tmp/level2/"
 
 mpirun -n $num_workers \
-    python3 -m src.level2.main mappings/level2/mast.yml -c ./configs/level2.csd3.yml --shot-min 11695 --shot-max 30474 -e camera_visible
+    python3 -m src.level2.main mappings/level2/mast.yml -c ./configs/level2.csd3.yml --shot-min 11695 --shot-max 30474 -e camera_visible -o $output_dir
 
